8c18a2a0c2f75548bf45728fd489e5d7b5daa08f
roma jam <jam_roma@yahoo.com>
2017-06-04 13:26:06 +0300

    + Add rboot-main project files.

---
+++ rboot-main/main.c
@@ -0,0 +1,12 @@
+/*
+ * main.c
+ *
+ *  Created on: 4 θώνÿ 2017 γ.
+ *      Author: RomaJam
+ */
+
+
+int main(void)
+{
+
+}
---
+++ rboot-main/system_config.h
@@ -0,0 +1,15 @@
+/*
+ * system_config.h
+ *
+ *  Created on: 4 θώνÿ 2017 γ.
+ *      Author: RomaJam
+ */
+
+#ifndef SYSTEM_CONFIG_H_
+#define SYSTEM_CONFIG_H_
+
+
+
+
+
+#endif /* SYSTEM_CONFIG_H_ */

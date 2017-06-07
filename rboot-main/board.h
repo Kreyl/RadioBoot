9ec62d99e8e6a94f4cb59f4ab15e061932c5cbd6
roma jam <jam_roma@yahoo.com>
2017-06-07 23:03:59 +0300

    Change logic of project

---
+++ rboot-main/board.h
@@ -0,0 +1,15 @@
+/*
+ * board.h
+ *
+ *  Created on: 7 θώνÿ 2017 γ.
+ *      Author: RomaJam
+ */
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+
+
+
+
+#endif /* BOARD_H_ */
---
+++ rboot-main/config.h
@@ -0,0 +1,15 @@
+/*
+ * config.h
+ *
+ *  Created on: 7 θώνÿ 2017 γ.
+ *      Author: RomaJam
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+
+
+
+
+#endif /* CONFIG_H_ */
